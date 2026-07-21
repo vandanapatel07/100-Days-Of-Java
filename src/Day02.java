0a326bc00143c80f4dbac578c6547af47480194d
vandanapatel07 <vandanapatelhr9027@gmail.com>
2026-07-21 11:15:30 +0530

    Variables and Data Types

---
+++ src/Day02.java
@@ -0,0 +1,24 @@
+public class Day02 {
+    public static void main(String[] args){
+        //integer data type
+        int age = 20;
+
+        //Decimal data type
+        double marks = 85.5;
+
+        //Character data type
+        char grade = 'A';
+
+        //Boolean data type
+        boolean isStudent = true;
+
+        //String data type
+        String name = "Vandana";
+
+        System.out.println("Name : " +name);
+        System.out.println("Age : " +age);
+        System.out.println("Marks : " +marks);
+        System.out.println("Grade : " +grade);
+        System.out.println("Is Student : " +isStudent);
+    }
+}
