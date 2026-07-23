174db93dca7a93c7aa3faf04e77a959fab17bdca
vandanapatel07 <vandanapatelhr9027@gmail.com>
2026-07-23 14:45:16 +0530

    Conditional Statement

--- src/Day04.java
+++ src/Day04.java
@@ -1,24 +1,19 @@
 import java.util.Scanner;
 public class Day04 {
     static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter your marks: ");
-        int marks=sc.nextInt();
+        int marks = sc.nextInt();
 
-        //Check Pass or Fail
-        //if-else statement
+        //  Program 1 : Pass or Fail
 
-        if (marks >= 40){
+
+        if (marks >= 40) {
             System.out.println("Student is pass");
-        }
-        else {
+        } else {
             System.out.println("Student is fail");
         }
-
-        //Check Grade
-        //else-if ladder
-
-        if(marks >= 90){
+        if (marks >= 90) {
             System.out.println("Grade: A+");
         } else if (marks >= 80) {
             System.out.println("Grade: A");
@@ -26,8 +21,61 @@ public class Day04 {
             System.out.println("Grade: B");
         } else if (marks >= 60) {
             System.out.println("Grade: C");
-        } else{
+        } else {
             System.out.println("Grade: F");
         }
+
+        // Program 2 : Positive Or Negative Numbers...
+
+
+        System.out.println("Enter a number : ");
+        int number=sc.nextInt();
+
+        if (number > 0){
+            System.out.println("Number is Positive");
+        } else if (number < 0) {
+            System.out.println("Nummber is Negative");
+        }else {
+            System.out.println("Number is Zero");
+    }
+
+
+        // Program 3 : Even Or Odd Numbers...
+
+
+        System.out.println("Enter a number :");
+        int num=sc.nextInt();
+        if (num % 2 == 0){
+            System.out.println("Number is Even");
+        }else {
+            System.out.println("Number is Odd");
+        }
+
+
+        //Program 4 : Largest Of Three Numbers...
+
+
+        System.out.println("Enter First Number :");
+        int a=sc.nextInt();
+
+        System.out.println("Enter Second Number :");
+        int b=sc.nextInt();
+
+        System.out.println("Enter Third Number :");
+        int c=sc.nextInt();
+
+        if (a >= b && a >= c){
+            System.out.println("Largest Number Is :"+a);
+        } else if (b >= a && b >= c) {
+            System.out.println("Largest Number Is :"+b);
+        }else {
+            System.out.println("Largest Number Is :"+c);
+        }
+        sc.close();
+
     }
 }
+
+
+
+
